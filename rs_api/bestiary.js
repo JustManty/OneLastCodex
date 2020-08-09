0506389331550e9f32b51816310fa0c24c295c43
Tyler Roberts <tjroberts95@gmail.com>
2020-08-09 16:47:27 -0400

    Beginning work on the process to refresh lookup tables
    This is still a WIP.  The code from this commit does not yet function correctly.

---
+++ rs_api/bestiary.js
@@ -0,0 +1,171 @@
+const Logger = require('./../utils/logger.js');
+const https = require('https');
+const logger = require('./../utils/logger.js');
+
+class Area {
+  constructor(areaName) {
+    this.areaName = areaName;
+  }
+}
+
+class BeastData {
+  constructor(jsonObject) {
+    this.name = jsonObject.name;
+    this.id = jsonObject.id;
+    this.members = jsonObject.members;
+    this.weakness = jsonObject.weakness;
+    this.level = jsonObject.level;
+    this.lifepoints = jsonObject.lifepoints;
+    this.defence = jsonObject.defence;
+    this.attack = jsonObject.attack;
+    this.magic = jsonObject.magic;
+    this.ranged = jsonObject.ranged;
+    this.xp = jsonObject.xp;
+    this.slayerLevel = jsonObject.slayerLevel;
+    this.slayerCategory = jsonObject.slayerCategory;
+    this.size = jsonObject.size;
+    this.attackable = jsonObject.attackable;
+    this.aggressive = jsonObject.aggressive;
+    this.poisonous = jsonObject.poisonous;
+    this.description = jsonObject.description;
+    this.areas = [];
+    jsonObject.area.forEach((areaName) => this.areas.push(new Area(areaName)));
+  }
+}
+
+class Beast {
+  constructor(jsonObject) {
+    this.id = jsonObject.value;
+    this.name = jsonObject.label;
+  }
+}
+
+class SlayerCategory {
+  constructor(jsonObject) {
+    Object.getOwnPropertyNames(jsonObject).forEach((name) => {
+      this.name = name;
+      this.id = jsonObject[name];
+    });
+  }
+}
+
+class Weakness {
+  constructor(jsonObject) {
+    Object.getOwnPropertyNames(jsonObject).forEach((name) => {
+      this.name = name;
+      this.id = jsonObject[name];
+    });
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function getBeastData(beastId) {
+  var options = {
+    host: 'secure.runescape.com',
+    path: `/m=itemdb_rs/bestiary/beastData.json?beastid=${beastId}`,
+  };
+
+  Logger.debug(`Attempting to call https:\\\\${options.host + options.path}`);
+
+  https
+    .request(options, (res) => {
+      var data = [];
+
+      res.on('data', (chunkOfData) => {
+        data.push(chunkOfData);
+      });
+
+      res.on('end', () => {
+        if (data === []) {
+          return null;
+        }
+        var parsedData = JSON.parse(data.join(''));
+        var beastData = new BeastData(parsedData);
+        return beastData;
+      });
+    })
+    .end();
+}
+
+function getBeastsByLetter(letter) {
+  var beasts = [];
+
+  var options = {
+    host: 'secure.runescape.com',
+    path: `/m=itemdb_rs/bestiary/bestiaryNames.json?letter=${letter}`,
+  };
+
+  Logger.debug(`Attempting to call https:\\\\${options.host + options.path}`);
+  https
+    .request(options, (res) => {
+      var data = [];
+
+      res.on('data', (chunkOfData) => {
+        data.push(chunkOfData);
+      });
+
+      res.on('end', () => {
+        beasts = JSON.parse(data.join(''));
+        Logger.debug(`Gathered beasts: ${JSON.stringify(beasts)}`);
+      });
+    })
+    .end();
+
+  return beasts;
+}
+
+var bestiary = {
+  refreshAllBeastData: async () => {
+    var beasts = [];
+    var possibleStartingLetters = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'X',
+      'Y',
+      'Z',
+    ];
+
+    for (let i = 0; i < possibleStartingLetters.length; i++) {
+      beasts.push(getBeastsByLetter(possibleStartingLetters[i]));
+      await sleep(process.env.API_WAIT_TIME);
+    }
+
+    console.log(beasts);
+
+    for (const beast of beasts) {
+      console.log(JSON.stringify(beast));
+      if (beast.id != undefined) {
+        var beastData = getBeastData(beast.id);
+        Logger.debug(`Gathered beast data: ${beastData}`);
+        await sleep(process.env.API_WAIT_TIME);
+      }
+    }
+  },
+};
+
+module.exports = bestiary;
--- server.js
+++ server.js
@@ -1,31 +1,42 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
+const Express = require('express');
+const DotEnv = require('dotenv');
+const Morgan = require('morgan');
+const Colors = require('colors/safe');
+const Chron = require('chron');
+const Logger = require('./utils/logger.js');
+const { refreshAllBeastData } = require('./rs_api/bestiary.js');
 
 // Load env variables
-dotenv.config({ path: './config/config.env' });
+DotEnv.config({ path: './config/config.env' });
 
-const app = express();
+// Load logger settings
+DotEnv.config({ path: './config/logger.env' });
+
+const app = Express();
 
 // Dev loggin middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'Development') {
+  app.use(Morgan('dev'));
 }
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`.yellow
-      .bold
+  Logger.info(
+    `The server was started in ${process.env.ENV} mode on port ${PORT}.`
   )
 );
 
+if (process.env.REFRESH_DATABASE === 'True') {
+  Logger.info('Beginning to refresh the database with new data...');
+  refreshAllBeastData();
+  Logger.info('Finished refreshing the database with new data...');
+}
+
 // Handle Unhandled Promise Rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
+  Logger.error(`Error: ${err.message}`.red);
   // Close server and exit process
   server.close(() => process.exit(1));
 });
